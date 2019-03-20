8ea9c6125dcc1ff7a16b5f16a258c0fd70ff2e09
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-03-19 19:44:54 -0600

    creating titles for the card

--- main.js
+++ main.js
@@ -1,1 +1,87 @@
-console.log("Hello World");
+const products = [
+    {
+        title: 'product1',
+        description: 'blah blahblah',
+        imageUrl: 'http://www.gannett-cdn.com/-mm-/421fa88894207f6fd125837bfbea3a215d8338a7/c=972-205-2409-1017/local/-/media/2016/10/18/USATODAY/USATODAY/636124053572235005-101816orange-cat-thinkstock.jpg?width=3200&height=1680&fit=crop',
+        size: '4" x 4"',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: '04/01/2016'
+    },
+
+    {
+        title: 'product2',
+        description: 'blah blahblah',
+        imageUrl: 'http://www.gannett-cdn.com/-mm-/421fa88894207f6fd125837bfbea3a215d8338a7/c=972-205-2409-1017/local/-/media/2016/10/18/USATODAY/USATODAY/636124053572235005-101816orange-cat-thinkstock.jpg?width=3200&height=1680&fit=crop',
+        size: '4" x 4"',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: '04/01/2016',
+    },
+
+    {
+        title: 'product3',
+        description: 'blah blahblah',
+        imageUrl: 'http://www.gannett-cdn.com/-mm-/421fa88894207f6fd125837bfbea3a215d8338a7/c=972-205-2409-1017/local/-/media/2016/10/18/USATODAY/USATODAY/636124053572235005-101816orange-cat-thinkstock.jpg?width=3200&height=1680&fit=crop',
+        size: '4" x 4"',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: '04/01/2016',
+    },
+
+    {
+        title: 'product4',
+        description: 'blah blahblah',
+        imageUrl: 'http://www.gannett-cdn.com/-mm-/421fa88894207f6fd125837bfbea3a215d8338a7/c=972-205-2409-1017/local/-/media/2016/10/18/USATODAY/USATODAY/636124053572235005-101816orange-cat-thinkstock.jpg?width=3200&height=1680&fit=crop',
+        size: '4" x 4"',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: '04/01/2016',
+    },
+
+    {
+        title: 'product5',
+        description: 'blah blahblah',
+        imageUrl: 'http://www.gannett-cdn.com/-mm-/421fa88894207f6fd125837bfbea3a215d8338a7/c=972-205-2409-1017/local/-/media/2016/10/18/USATODAY/USATODAY/636124053572235005-101816orange-cat-thinkstock.jpg?width=3200&height=1680&fit=crop',
+        size: '4" x 4"',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: '04/01/2016',
+    },
+];
+
+const printToDom = (divId, textToPrint) => {
+    let selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+}
+
+const domStringBuilder = (products) => {
+    domString = "";
+    for(let i = 0; i < products.length; i++) {
+        domString += `<div class="card-body">`;
+        domString += `<row>`;
+        domString += `<div class="col-sm">`
+        domString += `<div class="title"><h2>${products[i].title}</h2></div>`;
+        domString += `</div>`;
+        domString += `</row>`;
+        domString += `</div>`;
+    
+    }
+    printToDom("titles", domString);
+}
+
+const init = () => {
+    domStringBuilder(products);
+}
+
+init();
